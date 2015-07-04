72d5d5377304aa634fa89106dc20d8f8eedcb617
Maik Merten <maikmerten@googlemail.com>
2015-07-04 12:08:36 +0200

    move rendering questions into a somewhat proper object

--- static/shared.js
+++ static/shared.js
@@ -1,57 +1,54 @@
-function displayQuestion(question, clickfunction) {
-	var optionlist = $("#optionlist");
-	optionlist.empty();
-
-	// generate entries of option list
-	for(var i = 0; i < question.options.length; ++i) {
-		var option = $("<li>");
-		option.attr("id", "option" + i);
-		optionlist.append(option);
-	}
-
-	$("#questiontext").text(question.text);
-
-	var permute = [];
-	for(var i = 0; i < question.options.length; ++i) {
-
-		// display answering options in random order
-		var idx = 0;
-		while(true) {
-			var rand = Math.floor(Math.random() * question.options.length);
-			if(!permute[rand]) {
-				idx = rand;
-				permute[idx] = true;
-				break;
-			}
-		}
-
-		var option = $("#option" + idx);
-
-		option.text(question.options[i]);
-		option.toggleClass("option", true);
-		option.data("originalindex", i);
-
-		var count = $("<span>");
-		count.attr("id", "optioncount" + idx);
-		count.toggleClass("optioncount", true);
-		option.append(count);
-
-		if(clickfunction) {
-			option.click(clickfunction);
-		}
-	}
-
-	var imgelem = $("#questionimage");
-	if(question.image) {
-		imgelem.attr("src", question.image);
-		imgelem.css("display", "block");
-	} else {
-		imgelem.css("display", "none");
+QuestionRenderer = function(topElementSelector) {
+	this.topElem = $(topElementSelector);
+	if(!this.topElem.length) {
+		throw "QuestionRenderer: could not find topElement with selector " + topElementSelector;
 	}
 
 }
 
-function revealCorrect() {
+QuestionRenderer.prototype.renderQuestion = function(question, clickfunction) {
+	
+	this.topElem.empty().append($("<div>").attr("id", "questiontext").text(question.text));
+
+	if(question.image) {
+		this.topElem.append($("<img>").attr("id", "questionimage").attr("src", question.image).css("display", "block"));
+	}
+
+	if(question.options && question.options.length) {
+		var optionlist = $("<ul>").attr("id", "optionlist");
+		this.topElem.append(optionlist);
+
+		// generate entries of option list
+		for(var i = 0; i < question.options.length; ++i) {
+			optionlist.append($("<li>").attr("id", "option" + i).toggleClass("option", true));
+		}
+
+		// display answering options in random order
+		var permute = [];
+		for(var i = 0; i < question.options.length; ++i) {
+			var idx = 0;
+			while(true) {
+				var rand = Math.floor(Math.random() * question.options.length);
+				if(!permute[rand]) {
+					idx = rand;
+					permute[idx] = true;
+					break;
+				}
+			}
+
+			var option = $("#option" + idx).text(question.options[i]).data("originalindex", i);
+			var count = $("<span>").attr("id", "optioncount" + idx).toggleClass("optioncount", true);
+			option.append(count);
+
+			if(clickfunction) {
+				option.click(clickfunction);
+			}
+		}
+	}
+
+}
+
+QuestionRenderer.prototype.revealAnswer = function() {
 	for(var i = 0; i < 999; ++i) {
 		var option = $("#option" + i);
 		if(!option.length) {
