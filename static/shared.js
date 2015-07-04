8fdc7214b17e5b4eab2e94645311f1b5b106e4d8
Maik Merten <maikmerten@googlemail.com>
2015-07-04 18:18:29 +0200

    major cleanup to the quizmaster. En-/Disable UI elements according to current state.

--- static/shared.js
+++ static/shared.js
@@ -8,87 +8,133 @@ QuizRenderer = function(topElementSelector) {
 	var optionselected = false;
 }
 
-QuizRenderer.prototype.renderQuestion = function(question, clickcallback) {
-	optionselected = false;
-	this.topElem.empty().append($("<div>").attr("id", "questiontext").text(question.text));
+QuizRenderer.prototype = {
 
-	if(question.image) {
-		this.topElem.append($("<img>").attr("id", "questionimage").attr("src", question.image).css("display", "block"));
-	}
+	renderQuestion: function(question, clickcallback) {
+		optionselected = false;
+		this.topElem.empty().append($("<div>").attr("id", "questiontext").text(question.text));
 
-	if(question.options && question.options.length) {
-		var optionlist = $("<ul>").attr("id", "optionlist");
-		this.topElem.append(optionlist);
-
-		// generate entries of option list
-		for(var i = 0; i < question.options.length; ++i) {
-			optionlist.append($("<li>").attr("id", "option" + i).toggleClass("option", true));
+		if(question.image) {
+			this.topElem.append($("<img>").attr("id", "questionimage").attr("src", question.image).css("display", "block"));
 		}
 
-		// display answering options in random order
-		var permute = [];
-		for(var i = 0; i < question.options.length; ++i) {
-			var idx = 0;
-			while(true) {
-				var rand = Math.floor(Math.random() * question.options.length);
-				if(!permute[rand]) {
-					idx = rand;
-					permute[idx] = true;
-					break;
+		if(question.options && question.options.length) {
+			var optionlist = $("<ul>").attr("id", "optionlist");
+			this.topElem.append(optionlist);
+
+			// generate entries of option list
+			for(var i = 0; i < question.options.length; ++i) {
+				optionlist.append($("<li>").attr("id", "option" + i).toggleClass("option", true));
+			}
+
+			// display answering options in random order
+			var permute = [];
+			for(var i = 0; i < question.options.length; ++i) {
+				var idx = 0;
+				while(true) {
+					var rand = Math.floor(Math.random() * question.options.length);
+					if(!permute[rand]) {
+						idx = rand;
+						permute[idx] = true;
+						break;
+					}
+				}
+
+				var option = $("#option" + idx).text(question.options[i]).data("originalindex", i);
+				var count = $("<span>").attr("id", "optioncount" + idx).toggleClass("optioncount", true);
+				option.append(count);
+
+				if(clickcallback) {
+					option.click(function() {
+						if(optionselected) {
+							return;
+						}
+						optionselected = true;
+		
+						var option = $(this).toggleClass("selected", true);
+						clickcallback(option.data("originalindex"));
+					});
 				}
 			}
+		}
 
-			var option = $("#option" + idx).text(question.options[i]).data("originalindex", i);
-			var count = $("<span>").attr("id", "optioncount" + idx).toggleClass("optioncount", true);
-			option.append(count);
+	},
 
-			if(clickcallback) {
-				option.click(function() {
-					if(optionselected) {
-						return;
-					}
-					optionselected = true;
-		
-					var option = $(this).toggleClass("selected", true);
-					clickcallback(option.data("originalindex"));
-				});
+	revealAnswer: function() {
+		optionselected = true;
+
+		for(var i = 0; i < 999; ++i) {
+			var option = $("#option" + i);
+			if(!option.length) {
+				break;
 			}
+
+			var originalidx = option.data("originalindex");
+			option.toggleClass("correct", originalidx == 0);
+			option.toggleClass("incorrect", originalidx != 0);
+		}
+	},
+
+	revealVote: function(votes) {
+		for(var i = 0; i < 999; ++i) {
+			var option = $("#option" + i);
+			if(!option.length) {
+				break;
+			}
+
+			var originalidx = option.data("originalindex");
+			var optioncount = $("#optioncount" + i);
+
+			var count = 0;
+			for(var key in votes) {
+				if(originalidx == votes[key]) {
+					count++;
+				}
+			}
+			optioncount.text(count + "");
 		}
 	}
-
 }
 
-QuizRenderer.prototype.revealAnswer = function() {
-	optionselected = true;
-
-	for(var i = 0; i < 999; ++i) {
-		var option = $("#option" + i);
-		if(!option.length) {
-			break;
-		}
-
-		var originalidx = option.data("originalindex");
-		option.toggleClass("correct", originalidx == 0);
-		option.toggleClass("incorrect", originalidx != 0);
-	}
+QuizHolder = function(questions) {
+	this.quest = questions;
+	this.pos = 0;
 }
 
-QuizRenderer.prototype.revealVote = function(votes) {
-	for(var i = 0; i < 999; ++i) {
-		var option = $("#option" + i);
-		if(!option.length) {
-			break;
-		}
+QuizHolder.prototype = {
 
-		var originalidx = option.data("originalindex");
-		var optioncount = $("#optioncount" + i);
+	size: function() {
+		return this.quest.length;
+	},
 
-		var count = 0;
-		for(var key in votes) {
-			if(originalidx == votes[key]) {
-				count++;
-			}
+	position: function() {
+		return this.pos;
+	},
+
+	current: function() {
+		return this.quest[this.pos];
+	},
+
+	hasNext: function() {
+		return this.pos < this.quest.length - 1;
+	},
+
+	next: function() {
+		if(this.hasNext()) {
+			this.pos++;
 		}
-		optioncount.text(count + "");
+		return this.current();
+	},
+
+	hasPrev: function() {
+		return this.size() > 0 && this.pos > 0;
+	},
+
+	prev: function() {
+		if(this.hasPrev()) {
+			this.pos--;
+		}
+		return this.current();
 	}
+	
 }
