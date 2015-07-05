a09df89485e6d83806261414bd8fd96223332df1
Maik Merten <maikmerten@googlemail.com>
2015-07-05 10:39:29 +0200

    Use "private" attributes for objects. Don't use the prototype attribute. This should be harmless, given we most likely won't have much inheritance goin on

--- static/shared.js
+++ static/shared.js
@@ -1,26 +1,23 @@
 QuizRenderer = function(topElementSelector) {
-	this.topElem = $(topElementSelector);
-	if(!this.topElem.length) {
+	var topElem = $(topElementSelector);
+	if(!topElem.length) {
 		throw "QuizRenderer: could not find topElement with selector " + topElementSelector;
 	}
 
 	var that = this;
 	var optionselected = false;
-}
-
-QuizRenderer.prototype = {
-
-	renderQuestion: function(question, clickcallback) {
+	
+	this.renderQuestion = function(question, clickcallback) {
 		optionselected = false;
-		this.topElem.empty().append($("<div>").attr("id", "questiontext").text(question.text));
+		topElem.empty().append($("<div>").attr("id", "questiontext").text(question.text));
 
 		if(question.image) {
-			this.topElem.append($("<img>").attr("id", "questionimage").attr("src", question.image).css("display", "block"));
+			topElem.append($("<img>").attr("id", "questionimage").attr("src", question.image).css("display", "block"));
 		}
 
 		if(question.options && question.options.length) {
 			var optionlist = $("<ul>").attr("id", "optionlist");
-			this.topElem.append(optionlist);
+			topElem.append(optionlist);
 
 			// generate entries of option list
 			for(var i = 0; i < question.options.length; ++i) {
@@ -58,9 +55,9 @@ QuizRenderer.prototype = {
 			}
 		}
 
-	},
+	}
 
-	revealAnswer: function() {
+	this.revealAnswer = function() {
 		optionselected = true;
 
 		for(var i = 0; i < 999; ++i) {
@@ -73,9 +70,9 @@ QuizRenderer.prototype = {
 			option.toggleClass("correct", originalidx == 0);
 			option.toggleClass("incorrect", originalidx != 0);
 		}
-	},
+	}
 
-	revealVote: function(votes) {
+	this.revealVote = function(votes) {
 		for(var i = 0; i < 999; ++i) {
 			var option = $("#option" + i);
 			if(!option.length) {
@@ -97,42 +94,40 @@ QuizRenderer.prototype = {
 }
 
 QuizHolder = function(questions) {
-	this.quest = questions;
-	this.pos = 0;
-}
+	var quest = questions;
+	var pos = 0;
 
-QuizHolder.prototype = {
 
-	size: function() {
-		return this.quest.length;
-	},
+	this.size = function() {
+		return quest.length;
+	}
 
-	position: function() {
-		return this.pos;
-	},
+	this.position = function() {
+		return pos;
+	}
 
-	current: function() {
-		return this.quest[this.pos];
-	},
+	this.current = function() {
+		return quest[pos];
+	}
 
-	hasNext: function() {
-		return this.pos < this.quest.length - 1;
-	},
+	this.hasNext = function() {
+		return pos < (quest.length - 1);
+	}
 
-	next: function() {
+	this.next = function() {
 		if(this.hasNext()) {
-			this.pos++;
+			pos++;
 		}
 		return this.current();
-	},
+	}
 
-	hasPrev: function() {
-		return this.size() > 0 && this.pos > 0;
-	},
+	this.hasPrev = function() {
+		return this.size() > 0 && pos > 0;
+	}
 
-	prev: function() {
+	this.prev = function() {
 		if(this.hasPrev()) {
-			this.pos--;
+			pos--;
 		}
 		return this.current();
 	}
